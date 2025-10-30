e6189b004ee20e504823a8e349cd5f64e80bfc68
GlossyPuma14533 <christofferhansen10@icloud.com>
2025-10-29 19:38:44 -0700

    First Exercise

---
+++ Excersise1.js
@@ -0,0 +1,10 @@
+let firstName = "Ryan";
+let lastName = "Smith";
+
+let sum = 2 + 5;
+let sub = 4;
+let ans = sum - sub;
+
+
+console.log(`${sum} - ${sub} = ${ans}` )
+console.log(`Hello ${firstName} ${lastName}`);
